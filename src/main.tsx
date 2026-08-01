f4e9e4d3deba2b5a01c65544237efc75cd79c5a2
charles-blouin <cha.blouin@gmail.com>
2026-08-01 14:19:01 -0400

    Charger les styles de navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './leaflet-base.css'
+import './navigation.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
